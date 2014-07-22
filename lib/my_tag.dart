bce1f8941712c6cc772ab185f2cbfe17f8845666
Keiichiro Ui <keiichiro.ui@gmail.com>
2014-07-23 00:32:22 +0900

    Add my solution

--- lib/my_tag.dart
+++ lib/my_tag.dart
@@ -8,6 +8,16 @@ class MyTagElement extends PolymerElement {
   ParagraphElement get p => this.querySelector('p');
 
   MyTagElement.created() : super.created() {
+    _printContent(); // print null because the contents are not attached
+  }
+
+  @override
+  attached() {
+    super.attached();
+    _printContent(); // print contents
+  }
+
+  void _printContent() {
     var pe = p;
     var t = (pe == null) ? null : pe.text;
     print('my-tag p: ${t}');
